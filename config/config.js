0598483ea3d6196081fc4edd271b66efdb72d75e
Aman Jain <aman9893@users.noreply.github.com>
2021-01-13 16:31:37 +0530

    Update config.js

--- config/config.js
+++ config/config.js
@@ -1,6 +1,7 @@
+
 var mysql = require('mysql');
 var connection = mysql.createConnection({
-    host: 'http://13.234.17.12',
+    host: 'http://13.234.17.12:5000',
     user: 'root',
     password: '12345',
     database: 'shopCart360',
