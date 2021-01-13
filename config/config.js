bac94e66dec93d72c10c6eecc4a343d90a6bd2d9
Aman Jain <aman9893@users.noreply.github.com>
2021-01-13 15:42:56 +0530

    Update config.js

--- config/config.js
+++ config/config.js
@@ -1,16 +1,16 @@
 var mysql = require('mysql');
 var connection = mysql.createConnection({
-    host: 'localhost',
+    host: 'http://13.234.17.12:5000',
     user: 'root',
     password: '12345',
     database: 'shopCart360',
-    multipleStatements: true
+    multipleStatements: true,
 });
 connection.connect(function(err) {
     if (!err) {
         console.log("Database is connected");
     } else {
-        console.log("Error while connecting with database");
+        console.log(err,"Error while connecting with database");
     }
 });
 
