f8c03bb4f0a98953072d8ba6533539a3b8a1fef0
Aman Jain <aman9893@users.noreply.github.com>
2021-01-13 17:07:32 +0530

    Update app.js

--- app.js
+++ app.js
@@ -4,28 +4,46 @@ const routes = require('./router/router')
 const path = require('path')
 const cors = require('cors')
 const bodyParser = require('body-parser')
-const swaggerUi = require('swagger-ui-express');
+const swaggerUi = require('swagger-ui-express')
 const swaggerJSDoc = require('swagger-jsdoc');
-const app = express();
-
-
-const port = 3306
-// 8000
-//3011
-app.set('views', path.join(__dirname, 'views'));
-app.set('view engine', 'html');
+const app = express()
+const port = '5000'
+app.set('views', path.join(__dirname,'views'));
+app.set('view engine','html');
 
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({
-  extended: true
+    extended: true
 }));
 
+// const swaggerDefinition = {
+//     info: {
+//       title: 'Test Development', 
+//       version: '1.0.0',
+//       description: 'A sample API', 
+//     },
+//     host: "localhost:8000",
+//     basePath: '/', 
+//   };
+  
+// const options = {
+//     swaggerDefinition: swaggerDefinition,
+//     explorer: true,
+//     apis: ['./router/*.js'], 
+//   };
+  
+//   const swaggerSpec = swaggerJSDoc(options);
+//   app.get('/swagger.json', (req, res) => {
+//     res.setHeader('Content-Type', 'application/json');
+//     res.send(swaggerSpec);
+//   });
+//   app.use(cors());
+//   app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerSpec));
 
+// app.use('/', routes)
 
-app.use('/', routes)
+// app.get('/', (req,res) => res.send('Hello World'))
 
-app.get('/test', (req, res) => res.send('Hello World'))
-
-app.listen(port, () => console.log('port is running successfully'))
+app.listen(port, ()=> console.log('port is running successfully'))
 
 module.exports = app;
