cf82d0334b14984d7c6f79c8a21a68a7be81bfca
Aman Jain <aman9893@users.noreply.github.com>
2021-01-14 13:14:57 +0530

    Update app.js

--- app.js
+++ app.js
@@ -39,7 +39,11 @@ app.use(bodyParser.urlencoded({
 //   });
   app.use(cors());
 //   app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerSpec));
-
+app.all('*', function(req, res, next) {
+   res.header("Access-Control-Allow-Origin", "*");
+   res.header("Access-Control-Allow-Headers", "X-Requested-With");
+   next();
+});
 app.use('/', routes)
 
 app.get('/test', (req,res) => res.send('Hello World'))
